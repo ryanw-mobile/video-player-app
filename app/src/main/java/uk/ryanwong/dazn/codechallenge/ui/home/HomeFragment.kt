0af44084c4e74a8b8cacf16f0923f64ba6d51279
Ryan Wong <ryan@rwmobi.com>
2021-09-28 03:32:22 +0100

    [chore] App: import starter project generated by Android Studio

---
+++ app/src/main/java/uk/ryanwong/dazn/codechallenge/ui/home/HomeFragment.kt
@@ -0,0 +1,45 @@
+package uk.ryanwong.dazn.codechallenge.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import uk.ryanwong.dazn.codechallenge.R
+import uk.ryanwong.dazn.codechallenge.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
