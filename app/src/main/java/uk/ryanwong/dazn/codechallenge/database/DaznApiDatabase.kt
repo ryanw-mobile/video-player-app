2905a5307894b9ff0f93dee318966d04413f52b3
Ryan Wong <ryan@rwmobi.com>
2021-09-29 16:04:49 +0100

    [feature] RoomDB: implement data object converter and database singleton

---
+++ app/src/main/java/uk/ryanwong/dazn/codechallenge/database/DaznApiDatabase.kt
@@ -0,0 +1,40 @@
+package uk.ryanwong.dazn.codechallenge.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import uk.ryanwong.dazn.codechallenge.network.models.Event
+import uk.ryanwong.dazn.codechallenge.network.models.Schedule
+
+@Database(entities = [Event::class, Schedule::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class DaznApiDatabase : RoomDatabase() {
+
+    // DAOs
+    abstract val eventsDao: EventsDao
+    abstract val scheduleDao: ScheduleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DaznApiDatabase? = null
+
+        fun getInstance(context: Context): DaznApiDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        DaznApiDatabase::class.java,
+                        "dazn_api_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
