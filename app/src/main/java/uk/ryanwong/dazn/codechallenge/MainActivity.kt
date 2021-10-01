2965d671961af01a51b9946697ca4d30e1d34fdb
Ryan Wong <ryan@rwmobi.com>
2021-10-01 19:40:53 +0100

    [refactor] MainActivity: remove redundant variables

--- app/src/main/java/uk/ryanwong/dazn/codechallenge/MainActivity.kt
+++ app/src/main/java/uk/ryanwong/dazn/codechallenge/MainActivity.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import uk.ryanwong.dazn.codechallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(
+            findNavController(R.id.nav_host_fragment_activity_main)
+        )
     }
 }
