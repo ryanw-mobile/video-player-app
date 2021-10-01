7c95092ecef1845c43be2eb1bd9b517844dbd83b
Ryan Wong <ryan@rwmobi.com>
2021-10-01 01:08:40 +0100

    [feature] MainActivity: remove ActionBar

--- app/src/main/java/uk/ryanwong/dazn/codechallenge/MainActivity.kt
+++ app/src/main/java/uk/ryanwong/dazn/codechallenge/MainActivity.kt
@@ -1,12 +1,10 @@
 package uk.ryanwong.dazn.codechallenge
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import uk.ryanwong.dazn.codechallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +20,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_events, R.id.navigation_schedule, R.id.navigation_nonetwork
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
