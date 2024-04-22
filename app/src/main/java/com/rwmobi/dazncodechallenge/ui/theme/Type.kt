1f3050059d746a8bc6dd0945de33106c99ff08c2
Ryan Wong <ryan@rwmobi.com>
2024-04-22 18:23:53 +0100

    [feature] App: add bold type fonts

--- app/src/main/java/com/rwmobi/dazncodechallenge/ui/theme/Type.kt
+++ app/src/main/java/com/rwmobi/dazncodechallenge/ui/theme/Type.kt
@@ -14,34 +14,34 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.rwmobi.dazncodechallenge.R
 
-val averageSansFontFamily = FontFamily(
+val daznAppFontFamily = FontFamily(
     Font(R.font.average_sans_regular, FontWeight.Light),
     Font(R.font.average_sans_regular, FontWeight.Light, FontStyle.Italic),
     Font(R.font.average_sans_regular, FontWeight.Normal),
     Font(R.font.average_sans_regular, FontWeight.Normal, FontStyle.Italic),
     Font(R.font.average_sans_regular, FontWeight.SemiBold),
     Font(R.font.average_sans_regular, FontWeight.SemiBold, FontStyle.Italic),
-    Font(R.font.average_sans_regular, FontWeight.Bold),
-    Font(R.font.average_sans_regular, FontWeight.Bold, FontStyle.Italic),
-    Font(R.font.average_sans_regular, FontWeight.ExtraBold),
-    Font(R.font.average_sans_regular, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.merriweather_sans_bold, FontWeight.Bold),
+    Font(R.font.merriweather_sans_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.merriweather_sans_extrabold, FontWeight.ExtraBold),
+    Font(R.font.merriweather_sans_extrabold_italic, FontWeight.ExtraBold, FontStyle.Italic),
 )
 
 val DefaultTypography = Typography()
 val appTypography = Typography(
-    displayLarge = DefaultTypography.displayLarge.copy(fontFamily = averageSansFontFamily),
-    displayMedium = DefaultTypography.displayMedium.copy(fontFamily = averageSansFontFamily),
-    displaySmall = DefaultTypography.displaySmall.copy(fontFamily = averageSansFontFamily),
-    headlineLarge = DefaultTypography.headlineLarge.copy(fontFamily = averageSansFontFamily),
-    headlineMedium = DefaultTypography.headlineMedium.copy(fontFamily = averageSansFontFamily),
-    headlineSmall = DefaultTypography.headlineSmall.copy(fontFamily = averageSansFontFamily),
-    titleLarge = DefaultTypography.titleLarge.copy(fontFamily = averageSansFontFamily),
-    titleMedium = DefaultTypography.titleMedium.copy(fontFamily = averageSansFontFamily),
-    titleSmall = DefaultTypography.titleSmall.copy(fontFamily = averageSansFontFamily),
-    bodyLarge = DefaultTypography.bodyLarge.copy(fontFamily = averageSansFontFamily),
-    bodyMedium = DefaultTypography.bodyMedium.copy(fontFamily = averageSansFontFamily),
-    bodySmall = DefaultTypography.bodySmall.copy(fontFamily = averageSansFontFamily),
-    labelLarge = DefaultTypography.labelLarge.copy(fontFamily = averageSansFontFamily),
-    labelMedium = DefaultTypography.labelMedium.copy(fontFamily = averageSansFontFamily),
-    labelSmall = DefaultTypography.labelSmall.copy(fontFamily = averageSansFontFamily),
+    displayLarge = DefaultTypography.displayLarge.copy(fontFamily = daznAppFontFamily),
+    displayMedium = DefaultTypography.displayMedium.copy(fontFamily = daznAppFontFamily),
+    displaySmall = DefaultTypography.displaySmall.copy(fontFamily = daznAppFontFamily),
+    headlineLarge = DefaultTypography.headlineLarge.copy(fontFamily = daznAppFontFamily),
+    headlineMedium = DefaultTypography.headlineMedium.copy(fontFamily = daznAppFontFamily),
+    headlineSmall = DefaultTypography.headlineSmall.copy(fontFamily = daznAppFontFamily),
+    titleLarge = DefaultTypography.titleLarge.copy(fontFamily = daznAppFontFamily),
+    titleMedium = DefaultTypography.titleMedium.copy(fontFamily = daznAppFontFamily),
+    titleSmall = DefaultTypography.titleSmall.copy(fontFamily = daznAppFontFamily),
+    bodyLarge = DefaultTypography.bodyLarge.copy(fontFamily = daznAppFontFamily),
+    bodyMedium = DefaultTypography.bodyMedium.copy(fontFamily = daznAppFontFamily),
+    bodySmall = DefaultTypography.bodySmall.copy(fontFamily = daznAppFontFamily),
+    labelLarge = DefaultTypography.labelLarge.copy(fontFamily = daznAppFontFamily),
+    labelMedium = DefaultTypography.labelMedium.copy(fontFamily = daznAppFontFamily),
+    labelSmall = DefaultTypography.labelSmall.copy(fontFamily = daznAppFontFamily),
 )
