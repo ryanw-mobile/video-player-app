39d649eabc3b6e6c8a29764e1804822b40ad5442
Ryan Wong <ryan@rwmobi.com>
2024-04-22 01:30:45 +0100

    [feature] App: import custom font

--- app/src/main/java/com/rwmobi/dazncodechallenge/ui/theme/Type.kt
+++ app/src/main/java/com/rwmobi/dazncodechallenge/ui/theme/Type.kt
@@ -8,34 +8,40 @@
 package com.rwmobi.dazncodechallenge.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.rwmobi.dazncodechallenge.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val averageSansFontFamily = FontFamily(
+    Font(R.font.average_sans_regular, FontWeight.Light),
+    Font(R.font.average_sans_regular, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.average_sans_regular, FontWeight.Normal),
+    Font(R.font.average_sans_regular, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.average_sans_regular, FontWeight.SemiBold),
+    Font(R.font.average_sans_regular, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.average_sans_regular, FontWeight.Bold),
+    Font(R.font.average_sans_regular, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.average_sans_regular, FontWeight.ExtraBold),
+    Font(R.font.average_sans_regular, FontWeight.ExtraBold, FontStyle.Italic),
+)
+
+val DefaultTypography = Typography()
+val appTypography = Typography(
+    displayLarge = DefaultTypography.displayLarge.copy(fontFamily = averageSansFontFamily),
+    displayMedium = DefaultTypography.displayMedium.copy(fontFamily = averageSansFontFamily),
+    displaySmall = DefaultTypography.displaySmall.copy(fontFamily = averageSansFontFamily),
+    headlineLarge = DefaultTypography.headlineLarge.copy(fontFamily = averageSansFontFamily),
+    headlineMedium = DefaultTypography.headlineMedium.copy(fontFamily = averageSansFontFamily),
+    headlineSmall = DefaultTypography.headlineSmall.copy(fontFamily = averageSansFontFamily),
+    titleLarge = DefaultTypography.titleLarge.copy(fontFamily = averageSansFontFamily),
+    titleMedium = DefaultTypography.titleMedium.copy(fontFamily = averageSansFontFamily),
+    titleSmall = DefaultTypography.titleSmall.copy(fontFamily = averageSansFontFamily),
+    bodyLarge = DefaultTypography.bodyLarge.copy(fontFamily = averageSansFontFamily),
+    bodyMedium = DefaultTypography.bodyMedium.copy(fontFamily = averageSansFontFamily),
+    bodySmall = DefaultTypography.bodySmall.copy(fontFamily = averageSansFontFamily),
+    labelLarge = DefaultTypography.labelLarge.copy(fontFamily = averageSansFontFamily),
+    labelMedium = DefaultTypography.labelMedium.copy(fontFamily = averageSansFontFamily),
+    labelSmall = DefaultTypography.labelSmall.copy(fontFamily = averageSansFontFamily),
 )
